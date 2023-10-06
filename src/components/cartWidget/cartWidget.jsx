e749d1720fdd719108cf7ea0645a02175d07cfd3
juanlezama1 <juanpablolezama92@gmail.com>
2023-10-06 03:55:40 -0300

    navbar

--- src/components/cartWidget/cartWidget.jsx
+++ src/components/cartWidget/cartWidget.jsx
@@ -1,8 +1,21 @@
+import { Avatar, Badge, Space } from 'antd';
+import {ShoppingCartOutlined} from '@ant-design/icons';
+
+
+
 const CartWidget = () => {
 
     return (
-        <p></p>
-    )
+
+  <Space size="middle">
+    <Badge count={5}>
+      <Avatar shape="none" size="large" icon={<ShoppingCartOutlined />} />
+    </Badge>
+    
+  </Space>
+);
+
+        // <a className="header_navbar_item" href="./pages/about_us.html">CARRITO DE COMPRAS</a>
 }
 
 export default CartWidget
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,18 +6,15 @@ const Navbar = () => {
     <>
     <header className="row">
     <div className="header_index col-12">
-    <a classNameName="header_image_link" href="index.html"><img className="header_image rounded" src="./images/footer_logo.png" alt="logo_naturally"/></a> 
+    <a classNameName="header_image_link" href="index.html"><img className="header_image rounded" src="../public/images/navbar-image.png" alt="logo_gamershop"/></a> 
     <nav className="header_navbar">
         <ul className="header_navbar_list">
-            <li><a className="header_navbar_item" href="./pages/menu.html">MENÚ COMPLETO</a></li>
-            <li><a className="header_navbar_item" href="./pages/build_your_salad.html">ARMÁ TU ENSALADA</a></li>
-            <li><a className="header_navbar_item" href="./pages/about_us.html">¿QUIÉNES SOMOS?</a></li>
-            <li><a className="header_navbar_item" href="./pages/contact.html">CONTACTO</a></li>
+            <li><a className="header_navbar_item" href="./pages/about_us.html">LOG IN</a></li>
+            <li><CartWidget /></li>
         </ul>
     </nav>
     </div>
     </header>
-    <CartWidget />
     </>
     )
 }
