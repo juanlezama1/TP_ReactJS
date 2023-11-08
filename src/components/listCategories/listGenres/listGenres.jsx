b596115bb72f5c1a7f6379736524df32b6b30231
juanlezama1 <juanpablolezama92@gmail.com>
2023-11-07 22:42:03 -0300

    actualizadas fotos de géneros

--- src/components/listCategories/listGenres/listGenres.jsx
+++ src/components/listCategories/listGenres/listGenres.jsx
@@ -5,18 +5,18 @@ const ListGenres = () => {
     return (
 
         <div className='row m-4'>
-            <ListSingleGenre genre={'Playstation 5'} image={'ps5.jpg'}/>
-            <ListSingleGenre genre={'Xbox Series X/S'} image={'xbox_x_s.jpg'}/>
-            <ListSingleGenre genre={'PC'} image={'pc.jpg'} developer={"Windows OS/Linux OS"}/>
-            <ListSingleGenre genre={'Nintendo Switch'} image={'switch.jpg'}/>
-            <ListSingleGenre genre={'Playstation 4'} image={'ps4.jpg'}/>
-            <ListSingleGenre genre={'Xbox One'} image={'xbox_one.webp'}/>
-            <ListSingleGenre genre={'Android - iOS'} image={'android_ios.webp'}/>
-            <ListSingleGenre genre={'PSP'} image={'psp.png'}/>
-            <ListSingleGenre genre={'Playstation 3'} image={'ps3.jpg'}/>
-            <ListSingleGenre genre={'Xbox 360'} image={'xbox_360.jpg'}/>
-            <ListSingleGenre genre={'Nintendo 3DS'} image={'3ds.webp'}/>
-            <ListSingleGenre genre={'Nintendo Wii'} image={'wii.png'}/>
+            <ListSingleGenre genre={'Acción'} image={'action.webp'}/>
+            <ListSingleGenre genre={'Aventuras'} image={'adventure.avif'}/>
+            <ListSingleGenre genre={'Indies'} image={'indie.jpg'}/>
+            <ListSingleGenre genre={'RPG'} image={'rpg.png'}/>
+            <ListSingleGenre genre={'Estrategia'} image={'strategy.jpg'}/>
+            <ListSingleGenre genre={'Shooters'} image={'shooters.jpg'}/>
+            <ListSingleGenre genre={'Simuladores'} image={'simulators.webp'}/>
+            <ListSingleGenre genre={'Puzzle'} image={'puzzle.jpeg'}/>
+            <ListSingleGenre genre={'Arcade'} image={'arcade.jpeg'}/>
+            <ListSingleGenre genre={'Multiplayer'} image={'multiplayer.webp'}/>
+            <ListSingleGenre genre={'Deportes'} image={'sports.webp'}/>
+            <ListSingleGenre genre={'Educativos'} image={'educational.jpg'}/>
         </div>
     )
 }
