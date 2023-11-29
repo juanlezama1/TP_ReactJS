bd3487ba4c2dac4da7c22038d8f55952f67c3055
juanlezama1 <juanpablolezama92@gmail.com>
2023-11-29 01:04:50 -0300

    listo el carrito

--- src/components/peripheralDetailContainer/peripheralBuy/peripheralBuy.jsx
+++ src/components/peripheralDetailContainer/peripheralBuy/peripheralBuy.jsx
@@ -2,13 +2,31 @@ import React, { useState } from 'react';
 import { useContext } from "react"
 import { CartContext } from "../../context/cartContext"
 import './peripheralBuy.css'
-import { Button, message } from 'antd';
+import { Button, message, Popconfirm } from 'antd'
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import { useNavigate } from "react-router-dom"
 
 const PeripheralBuy = ({id, stock, image, title, price}) => {
 
     const {setCartQty, setCart, cart_qty, cart} = useContext(CartContext)
     const [units, setUnits] = useState(1)
     const [messageApi5, contextHolder5] = message.useMessage();
+    const navigate = useNavigate ()
+
+    const buy_now = () => {
+
+        messageApi5.open({
+            type: 'loading',
+            content: 'Procesando compra...',
+            duration: 0,
+        });
+    
+        setCartQty(units)
+        setCart([{item_id: id, item_quantity: units, item_price: price, item_image: image, item_name: title, item_type: "peripheral"}])
+        
+        setTimeout(() => {navigate('/cart')}, 2000);
+        setTimeout(messageApi5.destroy, 2000);
+    }
 
     const add_tocart = () => {
 
@@ -65,7 +83,7 @@ const PeripheralBuy = ({id, stock, image, title, price}) => {
             })
         }
      
-        setTimeout(messageApi.destroy, 1500);
+        setTimeout(messageApi5.destroy, 1500);
     }
 
     return (
@@ -93,9 +111,11 @@ const PeripheralBuy = ({id, stock, image, title, price}) => {
                         AÑADIR AL CARRITO
                     </Button>
 
-                    <Button className="confirmation_button" type="primary" size='large'>
-                        COMPRAR YA
-                    </Button>
+                    <Popconfirm placement="bottom" title="Confirmar compra" description="¿Desea desechar carrito actual y proceder a la compra del accesorio?" onConfirm={buy_now} okText="Confirmo" cancelText="Cancelar" icon={<ShoppingCartOutlined style={{color: 'blue',}}/>}> 
+                        <Button className="confirmation_button" type="primary" size='large'>
+                                COMPRAR YA                        
+                        </Button>
+                    </Popconfirm>
                 </div>
             </div>
         </>
@@ -103,3 +123,6 @@ const PeripheralBuy = ({id, stock, image, title, price}) => {
 }
 
 export default PeripheralBuy
+
+
+
