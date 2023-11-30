f3ef29ef75aa9ae63f4d0fa54057ba5964fc74bd
juanlezama1 <juanpablolezama92@gmail.com>
2023-11-29 22:33:08 -0300

    total listo

--- src/components/cart/cartTotal/cartTotal.jsx
+++ src/components/cart/cartTotal/cartTotal.jsx
@@ -8,17 +8,21 @@ const CartTotal = () => {
 
     let total = 0
     cart.forEach ((product) => {total += product.item_price*product.item_quantity})
-    let total2 = total.toLocaleString()
-    let iva = total*(21/100).toFixed(2)
-    // let imp_pais = total.
+
+    let iva = total*(21/100)
+    let imp_pais = total*(30/100)
+    let imp_servdigitales = total*(8/100)
 
     return (
 
         <>
             <div className="total_sector">
-                <h2>Subtotal: $ {total2}</h2>
-                <h3>IVA (21%): {iva}</h3>
-                <h3>Imp. País (35%): {}</h3>
+                <h2>Subtotal: $ {total.toFixed(2)}</h2>
+                <h3>IVA (21%): $ {iva.toFixed(2)}</h3>
+                <h3>Imp. País (35%): {imp_pais.toFixed(2)}</h3>
+                <h3>Imp. Servicios Digitales (8%): {imp_servdigitales.toFixed(2)}</h3>
+
+                <h1 className="total">TOTAL: $ {total+iva+imp_pais+imp_servdigitales}</h1>
             </div>
         </>
     )
