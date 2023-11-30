3918366d3d73c509c1f203042e5186b1d34a5cfa
juanlezama1 <juanpablolezama92@gmail.com>
2023-11-30 15:17:14 -0300

    casi todo listo

--- src/components/finalResult/finalDescription/finalDescription.jsx
+++ src/components/finalResult/finalDescription/finalDescription.jsx
@@ -19,7 +19,23 @@ const FinalDescription = ({order_id}) => {
         
         (
           <div>
-            LLEGÓ EL ID
+            <div className="row background">
+              <div className="col-7 text-center">
+                <img style={{height: '500px'}} src="/public/images/mario.png" alt="order_confirm" />
+              </div>
+
+              <div className="col-5 d-flex align-items-center justify-content-end texto_gamer">
+                <div className="row">
+                  <div className="col-12">
+                    ORDEN <br/>PROCESADA <br/> EXITOSAMENTE!
+                  </div>
+
+                  <div className="col-12">
+                      <p>Nº transacción: {order_id.id}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </>
@@ -29,22 +45,6 @@ const FinalDescription = ({order_id}) => {
   export default FinalDescription;
 
 
-
-
-
-
-// @font-face {
-//   font-family: 'fuenteGamer';
-//   src: url('./cartEmpty.ttf') format('truetype');
-//   font-weight: normal;
-//   font-style: normal;
-// }
-
-// .texto_gamer {
-//   font-family: 'fuenteGamer';
-//   text-align: center;
-//   font-size: 60px;
-// }
 
 // .background {
 //   background-image: url('/public/images/mario-background2.jpg');
