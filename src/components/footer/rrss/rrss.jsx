e66b31dd4fe62df73397cd00f381d60589f728c6
juanlezama1 <juanpablolezama92@gmail.com>
2023-11-30 18:29:24 -0300

    pagina terminada, queda purgar errores

--- src/components/footer/rrss/rrss.jsx
+++ src/components/footer/rrss/rrss.jsx
@@ -4,10 +4,10 @@ const RRSS = () => {
         <div className="footer_div_followus col-lg-2">
             <h2 className="footer_title_rrss text-center">¡Seguinos!</h2>
             <div className="footer_rrss">
-                <a className="footer_rrss_link" href="https://www.instagram.com/thegamershop_oficial/" target="_blank"><img className="footer_rrss_image" src="/public/images/footer_instagram1.png" alt="instagram_icon"/></a>
-                <a className="footer_rrss_link" href="https://www.facebook.com/thegamershop.mining" target="_blank"><img className="footer_rrss_image" src="/public/images/footer_facebook1.png" alt="facebook_icon"/></a>
-                <a className="footer_rrss_link" href="https://www.tripadvisor.com.ar/Attraction_Review-g57122-d12804756-Reviews-Gamer_Planet-Sandy_Utah.html" target="_blank"><img className="footer_rrss_image" src="/public/images/footer_tripadvisor1.png" alt="tripadvisor_icon"/></a>
-                <a className="footer_rrss_link" href="https://twitter.com/GamerShopOnline" target="_blank"><img className="footer_rrss_image" src="/public/images/footer_twitter1.png" alt="twitter_icon"/></a>
+                <a className="footer_rrss_link" href="https://www.instagram.com/thegamershop_oficial/" target="_blank"><img className="footer_rrss_image" src="/public/images/rrss/footer_instagram1.png" alt="instagram_icon"/></a>
+                <a className="footer_rrss_link" href="https://www.facebook.com/thegamershop.mining" target="_blank"><img className="footer_rrss_image" src="/public/images/rrss/footer_facebook1.png" alt="facebook_icon"/></a>
+                <a className="footer_rrss_link" href="https://www.tripadvisor.com.ar/Attraction_Review-g57122-d12804756-Reviews-Gamer_Planet-Sandy_Utah.html" target="_blank"><img className="footer_rrss_image" src="/public/images/rrss/footer_tripadvisor1.png" alt="tripadvisor_icon"/></a>
+                <a className="footer_rrss_link" href="https://twitter.com/GamerShopOnline" target="_blank"><img className="footer_rrss_image" src="/public/images/rrss/footer_twitter1.png" alt="twitter_icon"/></a>
             </div>
         </div>
     )
--- src/components/footer/whatsapp/whatsapp.jsx
+++ src/components/footer/whatsapp/whatsapp.jsx
@@ -2,7 +2,7 @@ const Whatsapp = () => {
 
     return (
         <div className="whatsapp_button">
-            <a href="https://api.whatsapp.com/send?phone=541131443573&text=Hola%20GamerShop!" target="_blank"><img className= "whatsapp_image" src="/public/images/footer_whatsapp.png" alt="whatsapp_logo"/></a>
+            <a href="https://api.whatsapp.com/send?phone=541131443573&text=Hola%20GamerShop!" target="_blank"><img className= "whatsapp_image" src="/public/images/rrss/footer_whatsapp.png" alt="whatsapp_logo"/></a>
         </div>
     )
 }
